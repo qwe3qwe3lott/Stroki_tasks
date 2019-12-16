b30242fe63f524b90045ffce2a628dbe1339d3d4
temp <temp@STUD.UNV.SU>
2019-12-16 10:56:26 +0300

    release v1.1

--- Stroki_task3.java
+++ Stroki_task3.java
@@ -1,28 +1,32 @@
-import java.util.Scanner;
+﻿import java.util.Scanner;
 import java.util.ArrayList;
 public class Stroki_task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        int count = 0;
-        int countMax = 0;
-        boolean f = false;
-        Character[] word = new Character[100];
-        for(int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' '){
-                word[count] = str.charAt(i);
-                count++;
+        ArrayList word = new ArrayList();
+        ArrayList finalWord = new ArrayList();
+        char[] c = in.nextLine().toCharArray();
+        int count;
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(word.size());
+            System.out.println(finalWord.size());
+            if (c[i] != ' ') {
+                word.add(c[i]);
             } else {
-                if (count ) {
+                if(word.size() >= finalWord.size()) {
+                    count = word.size() - finalWord.size();
+                    for(int j = 0; j < finalWord.size(); j++){
+                        finalWord.set(j, word.get(j));
+                    }
+                    for(int j = finalWord.size(); j < count; j++){
+                        finalWord.add(word.get(j));
+                    }
                 }
-                count = 0;
-            }
-            if(str.charAt(i) != ' '){
-                f = true;
-            } else {
-                f = false;
+                word.clear();
             }
         }
-        //не дописанно
+        for(int i = 0; i < finalWord.size(); i++) {
+            System.out.print(finalWord.get(i));
+        }
     }
 }
